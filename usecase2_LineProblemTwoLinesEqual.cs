8dd2db233821b0918cfb30187a9749133ebf2f10
palle146github <112770521+palle146github@users.noreply.github.com>
2022-09-28 11:09:02 +0530

    Add files via upload

---
+++ usecase2_LineProblemTwoLinesEqual.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineEqualTest
+{
+    class Program
+    {
+        static void Equalcheck(double a1, double a2, double b1, double b2, double c1,  double c2)
+        {
+            if (a1 / a2 == b1 / b2  && a1 / a2 == c1 / c2 )
+                Console.WriteLine("The given straight lines are equal");
+
+            else
+                Console.WriteLine("The given straight lines are not equal");
+        }
+        static void Main(string[] args)
+        {
+            double a1 = 3, a2 = 6, b1 = 3, b2 = 4, c1 = 6,c2 = 3;
+            Equalcheck(a1, b1, c1, a2, b2, c2);
+            Console.Readonly();
+        }
+        
+    }
+}
+
+
+
